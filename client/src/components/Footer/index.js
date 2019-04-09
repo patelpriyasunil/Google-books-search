0d453cb5a1bd80c18e765a7a85bdf4e49c3cf044
Patelpriyasunil <patel.priyasunil@gmail.com>
2019-04-08 20:38:08 -0400

    hw

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer navbar-fixed-bottom has-background-tan text-center heading-subtitle">
+      Google-Book-Search
+    </footer>
+  );
+};
+
+export default Footer;
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,20 +1,22 @@
 import axios from "axios";
 
 export default {
-  // Gets all books
-  getBooks: function() {
-    return axios.get("/api/books");
+  // Gets books from the Google API
+  getBooks: function(q) {
+    return axios.get("/api/google", {
+      params: { q: "title:" + q }
+    });
   },
-  // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
+  // Gets all saved books
+  getSavedBooks: function() {
+    return axios.get("/api//books");
   },
-  // Deletes the book with the given id
+  // Deletes the saved book with the given id
   deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
+    return axios.delete("/api//books/" + id);
   },
-  // Saves a book to the database
+  // Saves an book to the database
   saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
+    return axios.post("/api//books", bookData);
   }
 };
